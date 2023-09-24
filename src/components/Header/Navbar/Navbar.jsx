02a2a7d37b20560d29cdcd696334bec459aff65c
ShaplaRani <shaplarani621@gmail.com>
2023-09-24 09:55:58 -0700

    added  the navbar section in this project

---
+++ src/components/Header/Navbar/Navbar.jsx
@@ -0,0 +1,60 @@
+import { NavLink } from "react-router-dom";
+
+
+const Navbar = () => {
+    return (
+        <div>
+
+            <nav className=" w-10/12 m-auto md:flex  justify-between items-center py-12  ">
+
+                  <div className="flex justify-center">
+                        <img src="/src/assets/Logo.png" alt="" /> 
+                  </div>
+
+                <ul className="flex gap-6 lg:gap-12 mt-10 justify-center md:mt-0 font-bold text-lg">
+                    <li>
+                        <NavLink
+                            to="/"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "text-red-500 underline" : ""
+                            }
+                        >
+                            Home
+                        </NavLink>
+
+                    </li>
+
+                    <li>
+                        <NavLink
+                            to="/donation"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "text-red-500 underline" : ""
+                            }
+                        >
+                            Donation
+                        </NavLink>
+
+
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/statistics"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "text-red-500 underline" : ""
+                            }
+                        >
+                            Statistics
+                        </NavLink>
+                     
+
+
+                    </li>
+                </ul>
+
+            </nav>
+
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/components/Layout/MainLayout.jsx
@@ -0,0 +1,11 @@
+
+ import Navbar from "../Header/Navbar/Navbar";
+const MainLayout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default MainLayout;
